d300da2ddc5e3112418dd92ad2d894283a89222e
smp4 <51273275+smp4@users.noreply.github.com>
2022-06-19 21:48:33 +0200

    add bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+bounce_height = 100 # metres
+bounces = 0
+
+while bounces < 10:
+    bounces = bounces + 1
+    bounce_height = bounce_height * 3 / 5
+    print(bounces, round(bounce_height, 4))
+
